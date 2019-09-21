d3fa1302cc84cd837eb0bafaea7d2aa831ece718
admin <you@example.com>
2019-09-21 23:10:18 +0800

    登陆和预约

--- dzzwzx/urls.py
+++ dzzwzx/urls.py
@@ -4,9 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.zc, name='zc'),
-    path('wx', views.wx),
+    # path('wx', views.wx),
     path('dl/', views.dl),
     path('dl_2/', views.dl_2),
     path('sendSms', views.sendSms),
     path('zhuce', views.zhuce),
+    path('yy/', views.yy),
+    path('yy2/', views.yy2),
 ]
--- dzzwzx/views.py
+++ dzzwzx/views.py
@@ -92,15 +92,16 @@ def dl_2(request):
         if qset0.手机号 == '':
             return redirect(models.react_url+'zhuce'+'?access_token='+access_token+'&refresh_token='+refresh_token)
         else:
-            return redirect(models.react_url+'contact'+'?access_token='+access_token+'&refresh_token='+refresh_token)
-    # res = '{"openid":'+openid+'}'
-    # response = HttpResponse(res)
-    # response["Access-Control-Allow-Origin"] = "*"
-    # response["Access-Control-Allow-Methods"] = "POST, GET, OPTIONS"
-    # response["Access-Control-Max-Age"] = "1000"
-    # response["Access-Control-Allow-Headers"] = "*"
-    # return response
-    # return redirect('http://www.baidu.com')
+            手机号 = qset0.手机号
+            姓名 = qset0.其它['姓名']
+            身份证号码 = qset0.其它['身份证号码']
+            return redirect(
+                models.react_url+'userInfo'+'?\
+                access_token='+access_token+\
+                '&refresh_token='+refresh_token+\
+                '&手机号='+手机号+\
+                '&姓名='+姓名+\
+                '&身份证号码='+身份证号码)
 
 def sendSms(request):
     try:
@@ -175,3 +176,49 @@ def zhuce(request):
         response["Access-Control-Max-Age"] = "1000"
         response["Access-Control-Allow-Headers"] = "*"
         return response
+
+#预约
+def yy(request):
+    from urllib import parse
+    myOtherUrl = parse.quote('https://wx.wuminmin.top/dzzwzx/yy2/')
+    my_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='+myConfig.wxyy_appid+'&redirect_uri='+myOtherUrl+'&response_type=code&scope=snsapi_userinfo&state=123#wechat_redirect'
+    return redirect(my_url)
+
+#预约认证
+def yy2(request):
+    js_code = request.GET['code']
+    url = 'https://api.weixin.qq.com/sns/oauth2/access_token'
+    payload = {'appid': myConfig.wxyy_appid, 'secret': myConfig.wxyy_scrit, 'code': js_code,
+                'grant_type': myConfig.wxyy_grant_type}
+    r = requests.get(url=url, params=payload)
+    print(r.text)
+    r_json = json.loads(r.text)
+    access_token = r_json['access_token']
+    refresh_token = r_json['refresh_token']
+    openid = r_json['openid']
+    qset0 = models.微信预约用户表.objects(openid=openid).first()
+    if qset0 == None:
+        models.微信预约用户表(
+            openid=openid,
+            access_token=access_token,
+            refresh_token=refresh_token
+        ).save()
+        return redirect(models.react_url+'zhuce'+'?access_token='+access_token+'&refresh_token='+refresh_token)
+    else:
+        qset0.update(
+            access_token=access_token,
+            refresh_token=refresh_token
+        )
+        if qset0.手机号 == '':
+            return redirect(models.react_url+'zhuce'+'?access_token='+access_token+'&refresh_token='+refresh_token)
+        else:
+            手机号 = qset0.手机号
+            姓名 = qset0.其它['姓名']
+            身份证号码 = qset0.其它['身份证号码']
+            return redirect(
+                models.react_url+'yuyue'+'?\
+                access_token='+access_token+\
+                '&refresh_token='+refresh_token+\
+                '&手机号='+手机号+\
+                '&姓名='+姓名+\
+                '&身份证号码='+身份证号码)
