39736bc6847ee1c8b5e0e67894ada5b3e891e1e0
wuminmin <buckwmm@qq.com>
2020-04-16 16:40:59 +0800

    1

---
+++ canteen_alliance/db.py
@@ -0,0 +1,8 @@
+from . import models, tool
+import myConfig
+import json
+import traceback
+import time
+from django.http import HttpResponse, FileResponse, JsonResponse
+
+def 
--- canteen_alliance/models.py
+++ canteen_alliance/models.py
@@ -20,7 +20,7 @@ wx_wx_info_d = {
     'mobile':'',
     "main_id":''
 }
-class wx_wx_info(Document): #微信信息表
+class wx_openid(Document): #微信信息表
     meta = {"db_alias": canteen_alliance}
     d = DictField(default={})
 
@@ -46,71 +46,52 @@ class wx_organization(Document): #组织表
     meta = {"db_alias": canteen_alliance}
     d = DictField(default={})
 
-class wx_organization_department_info(Document): #组织部门表
-    meta = {"db_alias": canteen_alliance}
-    d = DictField(default={})
+# class wx_organization_department_info(Document): #组织部门表
+#     meta = {"db_alias": canteen_alliance}
+#     d = DictField(default={})
 
-class wx_join_organization_apply(Document): #加入组织申请表
-    meta = {"db_alias": canteen_alliance}
-    d = DictField(default={})
+# class wx_join_organization_apply(Document): #加入组织申请表
+#     meta = {"db_alias": canteen_alliance}
+#     d = DictField(default={})
 
 
 wx_organization_match_user_d = {
     'main_id':'',
     'organization_main_id':'',
-    'role':{
-        'super_admin':'超级管理员', 
-        'nomarl_admin':'普通管理员', 
-        'user':'普通用户', 
-    },
-    'labor_attribute':{
-        'contract':'合同制',
-        'dispatch':'派遣制',
-        'third_party':'第三方',
-        'intern':'实习生',
-        'other':'其它'
-    }
+    'role':['超级管理员','普通管理员','普通用户'],
+    'labor_attribute':['合同制','派遣制','第三方','其它'],
+    'department'：['生产部门','销售部门','其它部门']
 }
 class wx_organization_match_user(Document): #组织和个人关系表
     meta = {"db_alias": canteen_alliance}
     d = DictField(default={})
 
-
 wx_supplier_match_user_d = {
     'main_id':'',
     'supplier_main_id':'',
-    'role':{
-        'super_admin':'超级管理员', 
-        'nomarl_admin':'普通管理员', 
-        'user':'普通用户', 
-    },
-    'labor_attribute':{
-        'contract':'合同制',
-        'dispatch':'派遣制',
-        'third_party':'第三方',
-        'intern':'实习生',
-        'other':'其它'
-    }
+   'role':['超级管理员','普通管理员','普通用户'],
+   'shop':['餐厅1','餐厅2']
+    'labor_attribute':['合同制','派遣制','第三方','其它'],
 }
 class wx_supplier_match_user(Document): #供应商和个人关系表
     meta = {"db_alias": canteen_alliance}
     d = DictField(default={})
 
-class wx_supplier_info(Document): #供应商信息
+class wx_supplier(Document): #供应商信息
     meta = {"db_alias": canteen_alliance}
     d = DictField(default={})
 
-class wx_organization_match_supplier(Document): #组织和供应商关联
-    meta = {"db_alias": canteen_alliance}
-    d = DictField(default={})
+# class wx_organization_match_supplier(Document): #组织和供应商关联
+#     meta = {"db_alias": canteen_alliance}
+#     d = DictField(default={})
 
-class wx_supplier_department_info(Document): #供应商部门表
-    meta = {"db_alias": canteen_alliance}
-    d = DictField(default={})
+# class wx_supplier_department_info(Document): #供应商部门表
+#     meta = {"db_alias": canteen_alliance}
+#     d = DictField(default={})
 
-class wx_goods_info(Document): #供应商某部门货物信息
-    meta = {"db_alias": canteen_alliance}
-    d = DictField(default={})
+# class wx_goods_info(Document): #供应商某部门货物信息
+#     meta = {"db_alias": canteen_alliance}
+#     d = DictField(default={})
 
 
 
