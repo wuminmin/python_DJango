187615af1f9bb4cc0beb832b0f546b6926fa0aa3
wuminmin <buckwmm@qq.com>
2020-05-16 14:52:55 +0800

    1

--- qyrd/db.py
+++ qyrd/db.py
@@ -1,10 +1,11 @@
+
+import pymongo
+import myConfig
+mongo_src = "mongodb://"+myConfig.name_qyrd+":"+myConfig.pwd_qyrd + \
+    "@"+myConfig.host+":"+myConfig.port_str+"/?authSource="+myConfig.db_qyrd
+myclient = pymongo.MongoClient(mongo_src)
+mydb = myclient[myConfig.name_qyrd]
 def 根据栏目查询文章列表(lan_mu):
-    import pymongo
-    import myConfig
-    mongo_src = "mongodb://"+myConfig.name_qyrd+":"+myConfig.pwd_qyrd + \
-        "@"+myConfig.host+":"+myConfig.port_str+"/?authSource="+myConfig.db_qyrd
-    myclient = pymongo.MongoClient(mongo_src)
-    mydb = myclient[myConfig.db]
     pipeline = [
          {
             '$match': {
