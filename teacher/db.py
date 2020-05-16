cec206fb7fbdc9d65867191feb9c0d95fc11dffe
wuminmin <buckwmm@qq.com>
2020-05-16 22:50:11 +0800

    1

--- teacher/db.py
+++ teacher/db.py
@@ -8,12 +8,32 @@ myclient = pymongo.MongoClient(mongo_src)
 mydb = myclient[myConfig.teacher_db]
 
 def teacher_base_info_insert(table_name,d):
-    tool.debug_print('teacher_base_info_insert--',d)
-    if table_name == 'teacher_base_info':
-        mydb.teacher_base_info.insert(d)
+    if table_name == '基本信息':
+        mydb.基本信息.insert(d)
         return True
-    elif table_name == 'teacher_work_info':
-        mydb.teacher_work_info.insert(d)
+    elif table_name == '工作信息':
+        mydb.工作信息.insert(d)
+        return True
+    elif table_name == '学历信息':
+        mydb.学历信息.insert(d)
+        return True
+    elif table_name == '工作履历':
+        mydb.工作履历.insert(d)
+        return True
+    elif table_name == '考核信息':
+        mydb.考核信息.insert(d)
+        return True
+    elif table_name == '家庭信息':
+        mydb.家庭信息.insert(d)
+        return True
+    elif table_name == '专业技术职务':
+        mydb.专业技术职务.insert(d)
+        return True
+    elif table_name == '职业资格名称':
+        mydb.职业资格名称.insert(d)
+        return True
+    elif table_name == '奖励情况':
+        mydb.奖励情况.insert(d)
         return True
     else:
         return False
@@ -49,22 +69,77 @@ def teacher_base_info_query_list(table_name,my_filter_list):
         '$match':match
     })
     if pipeline == []:
-        if table_name == 'teacher_base_info':
-            r = mydb.teacher_base_info.aggregate(pipeline).limit(100)
+        if table_name == '基本信息':
+            r = mydb.基本信息.aggregate(pipeline).limit(100)
+        elif table_name == '工作信息':
+            r = mydb.工作信息.aggregate(pipeline).limit(100)
+        elif table_name == '学历信息':
+            r = mydb.学历信息.aggregate(pipeline).limit(100)
+        elif table_name == '工作履历':
+            r = mydb.工作履历.aggregate(pipeline).limit(100)
+        elif table_name == '考核信息':
+            r = mydb.考核信息.aggregate(pipeline).limit(100)
+        elif table_name == '家庭信息':
+            r = mydb.家庭信息.aggregate(pipeline).limit(100)
+        elif table_name == '专业技术职务':
+            r = mydb.专业技术职务.aggregate(pipeline).limit(100)
+        elif table_name == '职业资格名称':
+            r = mydb.职业资格名称.aggregate(pipeline).limit(100)
+        elif table_name == '奖励情况':
+            r = mydb.奖励情况.aggregate(pipeline).limit(100)
         else:
             r = []
     else:
-        if table_name == 'teacher_base_info':
-            r = mydb.teacher_base_info.aggregate(pipeline)
+        if table_name == '基本信息':
+            r = mydb.基本信息.aggregate(pipeline)
+        elif table_name == '工作信息':
+            r = mydb.工作信息.aggregate(pipeline)
+        elif table_name == '学历信息':
+            r = mydb.学历信息.aggregate(pipeline)
+        elif table_name == '工作履历':
+            r = mydb.工作履历.aggregate(pipeline)
+        elif table_name == '考核信息':
+            r = mydb.考核信息.aggregate(pipeline)
+        elif table_name == '家庭信息':
+            r = mydb.基本信息.aggregate(pipeline)
+        elif table_name == '专业技术职务':
+            r = mydb.专业技术职务.aggregate(pipeline)
+        elif table_name == '职业资格名称':
+            r = mydb.职业资格名称.aggregate(pipeline)
+        elif table_name == '奖励情况':
+            r = mydb.奖励情况.aggregate(pipeline)
         else:
             r = []
     r_list = tool.objectid_to_json(r)
     return r_list
 
 def teacher_base_info_delete_by_key(table_name,key,value):
-    print('teacher_base_info_delete_by_key',table_name)
-    if table_name == 'teacher_base_info':
-        r = mydb.teacher_base_info.remove({key:value})
+    if table_name == '基本信息':
+        mydb.基本信息.remove({key:value})
+        return True
+    elif table_name == '工作信息':
+        mydb.工作信息.remove({key:value})
+        return True
+    elif table_name == '学历信息':
+        mydb.学历信息.remove({key:value})
+        return True
+    elif table_name == '工作履历':
+        mydb.工作履历.remove({key:value})
+        return True
+    elif table_name == '考核信息':
+        mydb.考核信息.remove({key:value})
+        return True
+    elif table_name == '家庭信息':
+        mydb.家庭信息.remove({key:value})
+        return True
+    elif table_name == '专业技术职务':
+        mydb.专业技术职务.remove({key:value})
+        return True
+    elif table_name == '职业资格名称':
+        mydb.职业资格名称.remove({key:value})
+        return True
+    elif table_name == '奖励情况':
+        mydb.奖励情况.remove({key:value})
         return True
     else:
         return False
@@ -72,13 +147,53 @@ def teacher_base_info_delete_by_key(table_name,key,value):
 def teacher_base_info_update_by_key(table_name,key,value,my_temp):
     if '_id' in my_temp:
         my_temp.pop('_id')
-    if table_name == 'teacher_base_info':
-        r = mydb.teacher_base_info.update(
+    if table_name == '基本信息':
+        r = mydb.基本信息.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '工作信息':
+        r = mydb.工作信息.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '学历信息':
+        r = mydb.学历信息.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '工作履历':
+        r = mydb.工作履历.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '考核信息':
+        r = mydb.考核信息.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '家庭信息':
+        r = mydb.家庭信息.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '专业技术职务':
+        r = mydb.专业技术职务.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '职业资格名称':
+        r = mydb.职业资格名称.update(
+            {key:value},
+            {'$set':my_temp}
+        )
+    elif table_name == '奖励情况':
+        r = mydb.奖励情况.update(
             {key:value},
             {'$set':my_temp}
         )
     else:
-        r = {}
+        r = {} 
     tool.debug_print(key,value,my_temp,r)
     if 'updatedExisting' in r:
         if r['updatedExisting']:
--- teacher/tool.py
+++ teacher/tool.py
@@ -37,39 +37,142 @@ def traceback_print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False):
     if flag:
         print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)
 #--------------------------------------------------
+# table_info_list = [
+#     {'id':1,'name':'基本信息','table_name':'teacher_base_info'},
+#     {'id':2,'name':'工作信息','table_name':'teacher_work_info'},
+#     {'id':3,'name':'学历信息','table_name':'teacher_education_info'},
+#     {'id':4,'name':'工作履历','table_name':'teacher_resume_info'},
+#     {'id':5,'name':'考核信息','table_name':'teacher_assessment_info'},
+#     {'id':6,'name':'家庭信息','table_name':'teacher_family_info'},
+#     {'id':7,'name':'专业技术职务','table_name':'teacher_profession_info'},
+#     {'id':8,'name':'职业资格名称','table_name':'teacher_occupation_info'},
+#     {'id':9,'name':'奖励情况','table_name':'teacher_reward_info'},
+# ]
 table_info_list = [
-    {'id':1,'name':'基本信息','table_name':'teacher_base_info'},
-    {'id':2,'name':'工作信息','table_name':'teacher_work_info'},
-    {'id':3,'name':'学历信息','table_name':'teacher_education_info'},
-    {'id':4,'name':'工作履历','table_name':'teacher_resume_info'},
-    {'id':5,'name':'考核信息','table_name':'teacher_assessment_info'},
-    {'id':6,'name':'家庭信息','table_name':'teacher_family_info'},
-    {'id':7,'name':'专业技术职务','table_name':'teacher_profession_info'},
-    {'id':8,'name':'职业资格名称','table_name':'teacher_occupation_info'},
-    {'id':9,'name':'奖励情况','table_name':'teacher_reward_info'},
+    {'id':1,'name':'基本信息','table_name':'基本信息'},
+    {'id':2,'name':'工作信息','table_name':'工作信息'},
+    {'id':3,'name':'学历信息','table_name':'学历信息'},
+    {'id':4,'name':'工作履历','table_name':'工作履历'},
+    {'id':5,'name':'考核信息','table_name':'考核信息'},
+    {'id':6,'name':'家庭信息','table_name':'家庭信息'},
+    {'id':7,'name':'专业技术职务','table_name':'专业技术职务'},
+    {'id':8,'name':'职业资格名称','table_name':'职业资格名称'},
+    {'id':9,'name':'奖励情况','table_name':'奖励情况'},
 ]
 
+# table_header_dict = {
+#     'teacher_base_info':[
+#         {'id':1,'name':'编号','key':'main_id'},
+#         {'id':2,'name':'姓名','key':'name'},
+#         {'id':3,'name':'性别','key':'gender'},
+#         {'id':4,'name':'出生日期','key':'birth_day'},
+#         {'id':5,'name':'民族','key':'nation'},
+#         {'id':6,'name':'身份证','key':'identity_number'},
+#         {'id':7,'name':'籍贯','key':'birth_area'},
+#         {'id':8,'name':'政治面貌','key':'political_status'},
+#         {'id':9,'name':'入党日期','key':'join_party_day'},
+#         {'id':10,'name':'参加工作日期','key':'join_work_day'},
+#         {'id':11,'name':'婚姻状况','key':'marital_status'},
+#         {'id':12,'name':'出生地','key':'birth_place'},
+#         {'id':13,'name':'户口所在地','key':'hu_kou_location'},
+#         {'id':14,'name':'办公电话','key':'work_phone'},
+#         {'id':15,'name':'手机号码','key':'cell_phone'},
+#         {'id':16,'name':'电子邮件','key':'email'},
+#         {'id':17,'name':'紧急联系人姓名','key':'emergency_contact_name'},
+#         {'id':18,'name':'紧急联系人电话','key':'emergency_contact_phone'},
+#     ]
+# }
+
 table_header_dict = {
-    'teacher_base_info':[
-        {'id':1,'name':'编号','key':'main_id'},
-        {'id':2,'name':'姓名','key':'name'},
-        {'id':3,'name':'性别','key':'gender'},
-        {'id':4,'name':'出生日期','key':'birth_day'},
-        {'id':5,'name':'民族','key':'nation'},
-        {'id':6,'name':'身份证','key':'identity_number'},
-        {'id':7,'name':'籍贯','key':'birth_area'},
-        {'id':8,'name':'政治面貌','key':'political_status'},
-        {'id':9,'name':'入党日期','key':'join_party_day'},
-        {'id':10,'name':'参加工作日期','key':'join_work_day'},
-        {'id':11,'name':'婚姻状况','key':'marital_status'},
-        {'id':12,'name':'出生地','key':'birth_place'},
-        {'id':13,'name':'户口所在地','key':'hu_kou_location'},
-        {'id':14,'name':'办公电话','key':'work_phone'},
-        {'id':15,'name':'手机号码','key':'cell_phone'},
-        {'id':16,'name':'电子邮件','key':'email'},
-        {'id':17,'name':'紧急联系人姓名','key':'emergency_contact_name'},
-        {'id':18,'name':'紧急联系人电话','key':'emergency_contact_phone'},
-    ]
+    '基本信息':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'姓名','key':'姓名'},
+        {'id':4,'name':'出生日期','key':'出生日期'},
+        {'id':5,'name':'民族','key':'民族'},
+        {'id':6,'name':'性别','key':'性别'},
+        {'id':7,'name':'籍贯','key':'籍贯'},
+        {'id':8,'name':'政治面貌','key':'政治面貌'},
+        {'id':9,'name':'入党日期','key':'入党日期'},
+        {'id':10,'name':'参加工作日期','key':'参加工作日期'},
+        {'id':11,'name':'婚姻状况','key':'婚姻状况'},
+        {'id':12,'name':'出生地','key':'出生地'},
+        {'id':13,'name':'户口所在地','key':'户口所在地'},
+        {'id':14,'name':'办公电话','key':'办公电话'},
+        {'id':15,'name':'手机号码','key':'手机号码'},
+        {'id':16,'name':'电子邮件','key':'电子邮件'},
+        {'id':17,'name':'紧急联系人姓名','key':'紧急联系人姓名'},
+        {'id':18,'name':'紧急联系人电话','key':'紧急联系人电话'},
+    ],
+    '工作信息':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'部门','key':'部门'},
+        {'id':4,'name':'岗位序列','key':'岗位序列'},
+        {'id':5,'name':'岗位','key':'岗位'},
+        {'id':6,'name':'现职时间','key':'现职时间'},
+        {'id':7,'name':'任现职岗位等级时间','key':'任现职岗位等级时间'},
+    ],
+    '学历信息':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'入学日期','key':'入学日期'},
+        {'id':4,'name':'毕业日期','key':'毕业日期'},
+        {'id':5,'name':'学校','key':'学校'},
+        {'id':6,'name':'专业','key':'专业'},
+        {'id':7,'name':'学历','key':'学历'},
+        {'id':8,'name':'学位','key':'学位'},
+        {'id':9,'name':'学习方式','key':'学习方式'},
+    ],
+    '工作履历':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'履历开始日期','key':'履历开始日期'},
+        {'id':4,'name':'履历结束日期','key':'履历结束日期'},
+        {'id':5,'name':'工作单位','key':'工作单位'},
+        {'id':6,'name':'所在部门','key':'所在部门'},
+        {'id':7,'name':'岗位','key':'岗位'},
+        {'id':8,'name':'职务','key':'职务'},
+    ],
+    '考核信息':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'考核开始日期','key':'考核开始日期'},
+        {'id':4,'name':'考核结束日期','key':'考核结束日期'},
+        {'id':5,'name':'考核等级','key':'考核等级'},
+        {'id':6,'name':'考核单位','key':'考核单位'},
+    ],
+    '家庭信息':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'称谓','key':'称谓'},
+        {'id':4,'name':'姓名','key':'姓名'},
+        {'id':5,'name':'生日','key':'生日'},
+        {'id':6,'name':'政治面貌','key':'政治面貌'},
+        {'id':7,'name':'工作单位','key':'工作单位'},
+        {'id':8,'name':'职务','key':'职务'},
+    ],
+    '专业技术职务':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'身份证','key':'身份证'},
+        {'id':3,'name':'技术职务名称','key':'技术职务名称'},
+        {'id':4,'name':'取得时间','key':'取得时间'},
+    ],
+    '职业资格名称':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'职业资格名称','key':'职业资格名称'},
+        {'id':3,'name':'取得时间','key':'取得时间'},
+        {'id':4,'name':'资格等级','key':'资格等级'},
+        {'id':5,'name':'批准单位','key':'批准单位'},
+    ],
+    '奖励情况':[
+        {'id':1,'name':'编号','key':'编号'},
+        {'id':2,'name':'奖励类别','key':'奖励类别'},
+        {'id':3,'name':'奖励机构','key':'奖励机构'},
+        {'id':4,'name':'奖励时间','key':'奖励时间'},
+        {'id':5,'name':'奖励事由','key':'奖励事由'},
+        {'id':6,'name':'奖励名称','key':'奖励名称'},
+    ],
 }
 
 def get_my_basic_filter_list(data,method_dict):
@@ -174,7 +277,7 @@ def get_my_basic_table_list(data,method_dict):
 def create_row(data,method_dict):
     data = json.loads(data)
     my_temp = data['my_temp']
-    my_temp['main_id'] = create_32_bit_main_id()
+    my_temp['编号'] = create_32_bit_main_id()
     for one in table_header_dict[method_dict['table_name']]:
         key = one['key']
         if key in my_temp:
@@ -184,26 +287,26 @@ def create_row(data,method_dict):
     if db.teacher_base_info_insert(method_dict['table_name'],my_temp):
         res_dict = {'code': 20000, 'data': {}, 'message': '新增成功'}
     else:
-        res_dict = {'code': 3, 'data': {}, 'message': '身份证号码已存在'}
+        res_dict = {'code': 3, 'data': {}, 'message': '新增失败'}
     return res_dict
 
 def delete_row(data,method_dict):
     data = json.loads(data)
     my_temp = data['my_temp']
-    if not 'main_id' in my_temp:
+    if not '编号' in my_temp:
         return {'code': 1, 'data': {}, 'message': '编号不存在!'}
-    main_id = my_temp['main_id']
-    if not db.teacher_base_info_delete_by_key(method_dict['table_name'],'main_id',main_id):
+    main_id = my_temp['编号']
+    if not db.teacher_base_info_delete_by_key(method_dict['table_name'],'编号',main_id):
         return {'code': 2, 'data': {}, 'message': '删除失败'}
     return {'code': 20000, 'data': {}, 'message': '删除成功'}
 
 def update_row(data,method_dict):
     data = json.loads(data)
     my_temp = data['my_temp']
-    if not 'main_id' in my_temp:
+    if not '编号' in my_temp:
         return {'code': 1, 'data': {}, 'message': '编号不存在!'}
-    main_id = my_temp['main_id']
-    if not db.teacher_base_info_update_by_key(method_dict['table_name'],'main_id',main_id,my_temp):
+    main_id = my_temp['编号']
+    if not db.teacher_base_info_update_by_key(method_dict['table_name'],'编号',main_id,my_temp):
         return {'code': 2, 'data': {}, 'message': '修改失败'}
     return {'code': 20000, 'data': {}, 'message': '修改成功'}
 
@@ -218,7 +321,7 @@ def import_excel_data(data,method_dict):
     data = json.loads(data)
     tableData = data['tableData']
     for one in tableData:
-        one['main_id'] = create_32_bit_main_id()
+        one['编号'] = create_32_bit_main_id()
         db.teacher_base_info_insert(method_dict['table_name'],one)
     return {'code':20000,'data':{},'message':method_dict['table_name']+'导入成功'}
 
